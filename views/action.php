c220aa68ab031e871e9dba3dab61da3cec4a6d01
pevzneri <38676208+pevzneri@users.noreply.github.com>
2018-04-28 18:20:35 -0400

    Update action.php

--- views/action.php
+++ views/action.php
@@ -4,6 +4,9 @@ print_r($_POST);
 exit;
 
 $MC_API_Token = file_get_contents('https://auth.exacttargetapis.com/v1/requestToken');
+$decoded = json_decode($MC_API_Token);
+$MCtoken = $decoded['accessToken'];
+
 
 
 ?>
